b3550e919722bfb4d1769d0af95776ae5cefb462
Bill Huang <a22687677@gmail.com>
2022-04-25 10:46:25 -0700

    Create ReverseWordsinaSentence.js

---
+++ ReverseWordsinaSentence.js
@@ -0,0 +1,14 @@
+//Reverse the order of words in a given sentence (an array of characters). 
+//Take the “Hello World” string for example:"Hello World"-->"World Hello"
+
+//Runtime complexity
+//linear, O(n), position of all the elements in the sentence is changed.
+
+//Memory complexity
+//Constant, O(1).
+
+let reverseWords = function(sentence) {     
+  let words = sentence.split(" ").reverse().join(" ");;
+  console.log(words)
+  return words;
+};

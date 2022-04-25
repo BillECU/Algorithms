699d2fe15b7605563fe597c02c0dc0e1d8352d91
Bill Huang <a22687677@gmail.com>
2022-04-25 10:08:05 -0700

    Update FindSumOfTwo.js

--- FindSumOfTwo.js
+++ FindSumOfTwo.js
@@ -1,6 +1,12 @@
 //Given an array of integers and a value, determine if there are any two integers in the array whose sum is equal to the given value. 
 //Return true if the sum exists and return false if it does not.
 
+//Runtime Complexity
+//Linear, O(n).
+
+//Memory Complexity
+//Linear, O(n).
+
 let findSumOfTwo = function(A, val) {
   let list = new Array();
   for(let i in A){
